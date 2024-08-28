5fee8e290ad68d389a98aad5e3c6e65cced460be
ilyashq <ilyasenaa24@gmail.com>
2024-08-28 22:53:43 +0100

    Configuration du serveur Eureka pour l'enregistrement et la découverte des microservices

---
+++ ConstructionXP/Projets/src/test/java/com/construction/Projets/ProjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construction.Projets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
