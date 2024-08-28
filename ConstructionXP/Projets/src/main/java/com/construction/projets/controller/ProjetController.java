09d6fdd014fbcfa251d88e36718b8d3e16f62a72
ilyashq <ilyasenaa24@gmail.com>
2024-08-28 23:22:02 +0100

    Ajout du contrôleur de gestion des projets

---
+++ ConstructionXP/Projets/src/main/java/com/construction/projets/controller/ProjetController.java
@@ -0,0 +1,43 @@
+package com.construction.projets.controller;
+
+import com.construction.projets.dto.ProjetDTO;
+import com.construction.projets.service.ProjetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projets")
+public class ProjetController {
+
+    @Autowired
+    private ProjetService projetService;
+
+    @GetMapping
+    public List<ProjetDTO> getAllProjets() {
+        return projetService.getAllProjets();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjetDTO> getProjetById(@PathVariable Long id) {
+        return ResponseEntity.ok(projetService.getProjetById(id));
+    }
+
+    @PostMapping
+    public ProjetDTO createProjet(@RequestBody ProjetDTO projetDTO) {
+        return projetService.createProjet(projetDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjetDTO> updateProjet(@PathVariable Long id, @RequestBody ProjetDTO projetDTO) {
+        return ResponseEntity.ok(projetService.updateProjet(id, projetDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProjet(@PathVariable Long id) {
+        projetService.deleteProjet(id);
+        return ResponseEntity.noContent().build();
+    }
+}
