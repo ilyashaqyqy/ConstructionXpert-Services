84286647a6d3c78f389365872fa30274442baa03
ilyashq <ilyasenaa24@gmail.com>
2024-08-29 16:09:59 +0100

    Ajout des DTO pour le service de gestion des tâches

---
+++ ConstructionXP/Tasks/src/main/java/com/construction/tasks/dto/TaskDTO.java
@@ -0,0 +1,18 @@
+package com.construction.tasks.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDTO {
+    private Long id;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private String status;
+    private String resources;
+
+
+}
